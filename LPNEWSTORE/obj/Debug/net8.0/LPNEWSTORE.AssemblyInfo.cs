68d590f39713072f417c16a669b24b175edd4e09
Joaco Alvarez <joaquinaalvarez91@gmail.com>
2025-11-03 13:36:55 -0300

    Modulo de Reportes

--- LPNEWSTORE/obj/Debug/net8.0/LPNEWSTORE.AssemblyInfo.cs
+++ LPNEWSTORE/obj/Debug/net8.0/LPNEWSTORE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LPNEWSTORE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3355ddbdf70643fe93e7596f2dd4e05d7c648de5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30423cac530de5c69eb5673e3c31ad139056b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LPNEWSTORE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LPNEWSTORE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
