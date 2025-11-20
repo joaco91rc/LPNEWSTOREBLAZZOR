4ed27a8df6ea9b6af731de88a30ae146d535f34c
Joaco Alvarez <joaquinaalvarez91@gmail.com>
2025-11-20 12:36:08 -0300

    Modulo Servicio tecnico Avances. Slector Forma Pago. Selector cabecera y productos

--- LPNEWSTORE/obj/Debug/net8.0/LPNEWSTORE.AssemblyInfo.cs
+++ LPNEWSTORE/obj/Debug/net8.0/LPNEWSTORE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LPNEWSTORE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bc4a717f002f4a6dcc6dcfafc3f322e3cceb4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e12935cd81591400e5657945e2cf0a84188d4893")]
 [assembly: System.Reflection.AssemblyProductAttribute("LPNEWSTORE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LPNEWSTORE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
